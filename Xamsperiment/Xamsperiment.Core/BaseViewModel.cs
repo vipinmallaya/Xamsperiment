0c0238b80509ee3e25b899d8356a42634d9b449a
VipinMallaya <vkgopinathan@live.com>
2020-08-01 13:16:37 +0530

    Adding new project files

---
+++ Xamsperiment/Xamsperiment.Core/BaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Xamsperiment.Core
+{
+    public class BaseViewModel
+    {
+    }
+}
---
+++ Xamsperiment/Xamsperiment/ViewModel/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Xamsperiment.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    { 
+        protected void OnPropertyChanged(string fieldName)
+        {
+            if(string.IsNullOrEmpty(fieldName))
+            {
+                if (MainThread.IsMainThread)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(fieldName));
+                }
+                else
+                {
+                    MainThread.BeginInvokeOnMainThread(() => PropertyChanged(this, new PropertyChangedEventArgs(fieldName));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
